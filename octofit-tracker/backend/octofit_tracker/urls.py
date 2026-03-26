73a8d998693597bf80697602ee65301387d041d6
rene-lombard <lombard.ernest.rene@gmail.com>
2026-03-26 11:53:36 +0000

    Add base URL configuration based on CODESPACE_NAME environment variable

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,3 +1,11 @@
+
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = None
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
