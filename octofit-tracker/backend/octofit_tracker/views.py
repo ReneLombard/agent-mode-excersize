2a92c9c411a71f470c29447572000cda29c0f26c
rene-lombard <lombard.ernest.rene@gmail.com>
2026-03-26 11:51:28 +0000

    Refactor api_root to streamline URL construction with full_url function

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -31,14 +31,24 @@ def api_root(request, format=None):
 	codespace_name = os.environ.get('CODESPACE_NAME')
 	if codespace_name:
 		base_url = f"https://{codespace_name}-8000.app.github.dev"
+		def full_url(path):
+			# Remove scheme/host if present in path
+			if path.startswith('http://') or path.startswith('https://'):
+				idx = path.find('/', 8)
+				if idx != -1:
+					path = path[idx:]
+				else:
+					path = '/'
+			return base_url + path
 	else:
-		base_url = request.build_absolute_uri('/')[:-1]
+		def full_url(path):
+			return request.build_absolute_uri(path)
 
 	return Response({
-		'users': base_url + reverse('user-list', request=request, format=format),
-		'teams': base_url + reverse('team-list', request=request, format=format),
-		'activities': base_url + reverse('activity-list', request=request, format=format),
-		'leaderboard': base_url + reverse('leaderboard-list', request=request, format=format),
-		'workouts': base_url + reverse('workout-list', request=request, format=format),
+		'users': full_url(reverse('user-list')), 
+		'teams': full_url(reverse('team-list')),
+		'activities': full_url(reverse('activity-list')),
+		'leaderboard': full_url(reverse('leaderboard-list')),
+		'workouts': full_url(reverse('workout-list')),
 	})
 
